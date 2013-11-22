c6b64bba89a3632a8757d618305f62cffc3519d9
Arseniy Pendryak <warrior@yandex-team.ru>
2013-11-23 01:34:08 +0400

    modified triangulation

--- include/cg/triangulation/triangulation.h
+++ include/cg/triangulation/triangulation.h
@@ -8,91 +8,10 @@
 
 #include <cg/primitives/point.h>
 #include <cg/operations/orientation.h>
+#include <cg/triangulation/cell_structure.h>
+#include <cg/triangulation/predicates.h>
 
 namespace cg {
-
-struct FaceHandle;
-struct EdgeHandle;
-struct VertexHandle;
-struct Triangulation;
-
-typedef boost::shared_ptr<VertexHandle> Vertex;
-typedef boost::shared_ptr<EdgeHandle> Edge;
-typedef boost::shared_ptr<FaceHandle> Face;
-
-struct VertexHandle
-{
-    bool inf;
-
-    point_2 point;
-    Edge edge;
-
-    friend struct Triangulation;
-
-public:
-    VertexHandle() : inf(true)
-    {}
-
-    VertexHandle(const point_2 &p) : inf(false), point(p)
-    {}
-
-public:
-
-    bool operator<(const VertexHandle &other)
-    {
-        if (inf == other.inf)
-        {
-            if (inf)
-            {
-                return false;
-            }
-            return point < other.point;
-        }
-        return inf;
-    }
-
-};
-
-struct EdgeHandle
-{
-    Vertex first_vertex;
-    Vertex second_vertex;
-    Edge twin;
-    Edge next;
-    Face face;
-
-    friend struct Triangulation;
-
-public:
-
-    EdgeHandle(Vertex first, Vertex second) : first_vertex(first), second_vertex(second)
-    {}
-
-};
-
-struct FaceHandle
-{
-    Edge edge;
-
-    friend struct Triangulation;
-
-public:
-
-    FaceHandle(Edge edge) : edge(edge)
-    {}
-};
-
-int ccw(int i)
-{
-    assert(i >= 0 && i < 3);
-    return (i + 1) % 3;
-}
-
-int cw(int i)
-{
-    assert(i >= 0 && i < 3);
-    return (i + 2) % 3;
-}
 
 struct Triangulation
 {
@@ -122,7 +41,7 @@ struct Triangulation
         v[1] = Vertex(new VertexHandle(b));
         v[2] = Vertex(new VertexHandle(c));
         Edge edges[3];
-        auto nextIndex = orientation(a, b, c) == cg::CG_LEFT ? ccw : cw;
+        auto nextIndex = orientation(a, b, c) == cg::CG_LEFT ? next : prev;
         for (int i = 0, j = 0; i < 3; i++, j = nextIndex(j))
         {
             edges[i] = Edge(new EdgeHandle(v[j], v[nextIndex(j)]));
@@ -130,7 +49,7 @@ struct Triangulation
         }
         for (int i = 0; i < 3; i++)
         {
-            edges[i]->next = edges[ccw(i)];
+            edges[i]->next = edges[next(i)];
         }
         Edge reverseEdges[3];
         for (int i = 0; i < 3; i++)
@@ -156,7 +75,7 @@ struct Triangulation
         }
         for (int i = 0; i < 3; i++)
         {
-            setTwins(reverseEdges[i]->next->next, reverseEdges[ccw(i)]->next);
+            setTwins(reverseEdges[i]->next->next, reverseEdges[next(i)]->next);
         }
         for (int i = 0; i < 3; i++)
         {
